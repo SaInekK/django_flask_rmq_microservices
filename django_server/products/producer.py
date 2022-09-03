8e492b5fcd5c76413e9f5a5a31ddba2c573410b2
SaInekK <samesainek@gmail.com>
2022-09-03 16:38:19 +0300

    Initial commit - products and likes communication completed

---
+++ django_server/products/producer.py
@@ -0,0 +1,21 @@
+import os
+import json
+
+import pika
+
+# params = pika.URLParameters(os.getenv("AMQP_URL"))
+# connection = pika.BlockingConnection(params)
+# channel = connection.channel()
+
+
+def publish(method, body):
+    params = pika.URLParameters(os.getenv("AMQP_URL"))
+    connection = pika.BlockingConnection(params)
+    channel = connection.channel()
+
+    if not connection or connection.is_closed:
+        connection = pika.BlockingConnection(params)
+
+        channel = connection.channel()
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
